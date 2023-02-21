2e9638d11f0b8574147df59b79a4e8c3d77dc8c5
sarala-246 <125871191+sarala-246@users.noreply.github.com>
2023-02-21 23:06:51 +0530

    Add files via upload

---
+++ newfile.go
@@ -0,0 +1,89 @@
+// Calling function multiple times
+
+// package main
+// import ("fmt")
+
+// func myMessage() {
+//   fmt.Println("Welcome to Go!")
+// }
+
+// func main() {
+//   myMessage()
+//   myMessage()
+//   myMessage()
+// }
+
+
+// Functions with arguments
+
+// package main
+// import "fmt"
+
+// func main(){
+// 	fmt.Println("starts func")
+// 	gm()
+// 	ga()
+// 	ge()
+// 	DisplayMessage("welcome to the golang world")
+// 	DisplayMessage("Hello")
+// 	DisplayMessage("team")
+// 	fmt.Println("Ends func")
+	
+// 	}
+
+
+// func ge(){
+// 	fmt.Println("good evening all of you")
+	
+// 	}
+	
+
+// func gm(){
+// 	fmt.Println("good morning all of you")
+	
+// 	}
+	
+// func ga(){
+// 	fmt.Println("good afternoon all of you")
+	
+// 	}
+
+
+// func DisplayMessage(s string) {
+
+// 	fmt.Println("the message is :",s)
+		
+// }
+
+
+// fuctions with return type
+
+// package main
+// import "fmt"
+// func main(){
+// 	var res int
+// 	res=add(100,30)
+// 	fmt.Println("the addition of two values :",res)
+// 	}
+	
+// func add(x int,y int) int {
+// 	var z int
+// 	z=x+y
+// 	return z
+// }
+
+
+// Function with multiple parameters
+
+// package main
+// import ("fmt")
+
+// func familyName(fname string, age int) {
+//   fmt.Println("Hello", age, "year old", fname)
+// }
+
+// func main() {
+//   familyName("sarala", 5)
+//   familyName("Jenny", 10)
+//   familyName("Anjali", 10)
+// }
