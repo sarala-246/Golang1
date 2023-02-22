0bc4a473b812ca7f0750f02cfe537c9ea569fbc5
sarala-246 <125871191+sarala-246@users.noreply.github.com>
2023-02-22 22:46:54 +0530

    Add files via upload

--- newfile.go
+++ newfile.go
@@ -87,3 +87,72 @@
 //   familyName("Jenny", 10)
 //   familyName("Anjali", 10)
 // }
+
+/////////////////////////////////////////
+
+// package main
+
+// import "fmt"
+
+// func main() {
+
+// 	var arr [4]int
+// 	fmt.Println("Elements of the Array: ", arr)
+
+// }
+
+///////////////////////////////////////////////////////////////////
+
+
+
+// package main
+  
+// import "fmt"
+  
+// func main() {
+  
+//     arr := [4][4]int{{1,2,3,4}, {10,100,110,120},
+//         {20,30,50,70}, {3,6,9,7}}
+  
+//     fmt.Println("Elements of Array",arr)
+// }
+
+
+
+////////////////////////////////////////////////////////////////
+
+
+
+// 1 - dimensional array
+
+
+// package main
+  
+// import "fmt"
+  
+// func main() {
+  
+// arr:= [5]int{10,20,30,40,50}
+
+// fmt.Println("Elements of the array:",arr)
+
+// }
+
+
+///////////////////////////////////////////////////////////////////
+
+
+// package main
+  
+// import "fmt"
+  
+// func main() {
+     
+// arr1:= [2]string{"sarala", "Tammireddy"}
+// arr2:= [3]string{"Hello", "Welcome", "Golang"}
+// arr3:= [...]int{1,2,3,4,5,6,}
+  
+// fmt.Println("Length of the array 1 is:", len(arr1), arr1)
+// fmt.Println("Length of the array 2 is:", len(arr2), arr2)
+// fmt.Println("Length of the array 3 is:", len(arr3), arr3)
+// }
